3a3c989e4bcb6f152333670f2a9dd655b22b4c01
Jon Quarfoth <jon.quarfoth@gmail.com>
2017-01-16 13:08:31 -0600

    add edit mode

--- src/main/resources/static/js/todo-item.component.js
+++ src/main/resources/static/js/todo-item.component.js
@@ -17,7 +17,30 @@
 
         function TodoItemCtrl() {
             var vm = this;
+            vm.editMode = false;
+            vm.editTask = '';
             vm.checkboxToggle = checkboxToggle;
+            vm.edit = edit;
+            vm.saveEdit = saveEdit;
+            vm.cancelEdit = cancelEdit;
+
+            function edit() {
+                vm.editTask = vm.todo.task;
+                vm.editMode = true;
+            }
+
+            function cancelEdit() {
+                vm.editTask = '';
+                vm.editMode = false;
+            }
+
+            function saveEdit() {
+                vm.todo.task = vm.editTask;
+                vm.onUpdate().then(function() {
+                    vm.editMode = false;
+                    vm.editTask = '';
+                });
+            }
 
             function checkboxToggle() {
                 vm.todo.done = !vm.todo.done;
